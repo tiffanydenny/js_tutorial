70abd896f058af5334c2d2c72a7202a850dadfff
Tiffany Denny <tiffany@tiffanydenny.com>
2020-03-23 17:07:46 -0500

    Change input and result to be on the page

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 let Phrase = require("tdenny-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-  } else {
-    alert(`"${phrase.content}" is not a palindrome.`);
+function palindromeTester(event) {
+  event.preventDefault();
+
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+    } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
   }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
+  });
+});
